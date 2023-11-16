9a76f2a90997cd371886d05d373aecdfc7c5780e
Gabriela Porto <code>
2023-11-16 19:01:50 -0300

    feature: crop images

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,7 +4,6 @@ import shutil
 import cv2
 import numpy as np
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data')))
-# from sklearn.model_selection import StratifiedKFold
 
 RAW_IMAGES_DIR = "data/raw/input/imagens_ihq_er/"
 RAW_ORGANIZED_DIR = "data/raw/organized/imagens_ihq_er"
@@ -12,19 +11,40 @@ CROPPED_IMAGES_DIR = "data/processed/cropped_images/"
 FOLDS_DIR = "data/processed/folded_data/"
 CROP_SIZE = (40, 30)
 
-def organize_images(input_dir=RAW_IMAGES_DIR, output_dir=RAW_ORGANIZED_DIR):
-    
-    for classification in os.listdir(input_dir):
-        classification_path = os.path.join(input_dir, classification)
-        if os.path.isdir(classification_path):
-            for image_name in os.listdir(classification_path):
-                patient_id = image_name.split('_')[0]
-                patient_dir = os.path.join(output_dir, classification, patient_id)            
-                os.makedirs(patient_dir, exist_ok=True)
-                src = os.path.join(classification_path, image_name)
-                dest = os.path.join(patient_dir, image_name)
-                shutil.copy(src, dest)
+def organize_images( ):
+    for classification in os.listdir(RAW_IMAGES_DIR):
+        classification_path = os.path.join(RAW_IMAGES_DIR, classification)
+        for image_name in os.listdir(classification_path):
+            patient_id = image_name.split('_')[0]
+            patient_dir = os.path.join(RAW_ORGANIZED_DIR, classification, patient_id)            
+            os.makedirs(patient_dir, exist_ok=True)
+            src = os.path.join(classification_path, image_name)
+            dest = os.path.join(patient_dir, image_name)
+            shutil.copy(src, dest)
 
+def crop_images():
+    # Para cada diretório de classificação
+    for classification in os.listdir(RAW_ORGANIZED_DIR):
+        classification_path = os.path.join(RAW_ORGANIZED_DIR, classification)
+        # Para cada diretório de paciente
+        for patient_id in os.listdir(classification_path):
+            cropped_image_dir = os.path.join(CROPPED_IMAGES_DIR, classification, patient_id)
+            os.makedirs(cropped_image_dir, exist_ok=True)
+            patient_path = os.path.join(classification_path, patient_id)
+            # Para cada imagem no diretório do paciente
+            for image_name in os.listdir(patient_path):
+                image_path = os.path.join(patient_path, image_name)
+                image = cv2.imread(image_path)
+                if image is not None:
+                    # Crop and save the image
+                    for i in range(0, image.shape[0], CROP_SIZE[0]):
+                        for j in range(0, image.shape[1], CROP_SIZE[1]):
+                            cropped_image = image[i:i+CROP_SIZE[0], j:j+CROP_SIZE[1]]
+                            cropped_image_name = f"{image_name}_crop_{i}_{j}.png"
+                            cv2.imwrite(os.path.join(cropped_image_dir, cropped_image_name), cropped_image)
+                else:
+                    print(f"Error reading image: {image_path}")
+                        
 # def crop_image(image_path, output_path=CROPPED_IMAGES_DIR, crop_size=CROP_SIZE):
 #     """
 #     Crop an image into smaller images of specified size and save them.
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,9 +1,10 @@
 import unittest
 import sys
 import os
+import cv2
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
-from preprocessing import organize_images, RAW_IMAGES_DIR, RAW_ORGANIZED_DIR
+from preprocessing import organize_images, crop_images, RAW_IMAGES_DIR, RAW_ORGANIZED_DIR, CROPPED_IMAGES_DIR,  CROP_SIZE
 
 class TestOrganizeImages(unittest.TestCase):
     def test_directories_created(self):
@@ -17,5 +18,24 @@ class TestOrganizeImages(unittest.TestCase):
                     patient_dir = os.path.join(RAW_ORGANIZED_DIR, classification, patient_id)
                     self.assertTrue(os.path.isdir(patient_dir))
                     
+class TestCropImages(unittest.TestCase):
+    def test_images_cropped_and_saved(self):
+        crop_images()
+
+        for classification in os.listdir(RAW_ORGANIZED_DIR):
+            for patient_id in os.listdir(os.path.join(RAW_ORGANIZED_DIR, classification)):
+                patient_path = os.path.join(RAW_ORGANIZED_DIR, classification, patient_id)
+                for image_name in os.listdir(patient_path):
+                    image_path = os.path.join(patient_path, image_name)
+                    image = cv2.imread(image_path)
+                    cropped_image_dir = os.path.join(CROPPED_IMAGES_DIR, classification, patient_id)
+                    self.assertTrue(os.path.isdir(cropped_image_dir))
+                    for i in range(0, image.shape[0], CROP_SIZE[0]):
+                        for j in range(0, image.shape[1], CROP_SIZE[1]):
+                            cropped_image_name = f"{image_name}_crop_{i}_{j}.png"
+                            cropped_image_path = os.path.join(cropped_image_dir, cropped_image_name)
+                            self.assertTrue(os.path.exists(cropped_image_path))
+                            
+         
 if __name__ == '__main__':
     unittest.main()
